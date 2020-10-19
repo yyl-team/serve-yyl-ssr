43dcc5c126d1be27c6589218abc7e7989353b851
liudaojie <liudaojie@yy.com>
2020-10-19 18:59:43 +0800

    feat: 开发中

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,19 @@
-type ServeYylSsr = () => {}
+/** render option */
+interface ServeYylSsrOptionRenderOption {
+  res: Response
+  req: Request
+  next: (err?: any) => void
+}
+/** serveYylSss option */
+interface ServeYylSsrOption {
+  /** 渲染 */
+  render(op: ServeYylSsrOptionRenderOption)
+  /** 是否处于生产环境 */
+  isProduct?: boolean
+  /** 缓存持续时间 */
+  cacheExpire?: number
+}
+type ServeYylSsr = (op: ServeYylSsrOption) => {}
 
 declare const serveYylSsr: ServeYylSsr
 export = serveYylSsr
