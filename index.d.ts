09cf729b5903012e94e4d878972017448d7509d0
liudaojie <liudaojie@yy.com>
2020-10-19 23:40:45 +0800

    feat: 开发完成，缺自测用例

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,23 @@
 /** render option */
-interface ServeYylSsrOptionRenderOption {
+export interface ServeYylSsrOptionRenderOption {
   res: Response
   req: Request
   next: (err?: any) => void
 }
+export type LoggerType = 'info' | 'error' | 'warn' | 'success'
+export type Logger = (type: LoggerType, args: any[]) => void
 /** serveYylSss option */
-interface ServeYylSsrOption {
+export interface ServeYylSsrOption {
   /** 渲染 */
-  render(op: ServeYylSsrOptionRenderOption)
+  render(op: ServeYylSsrOptionRenderOption): Promise<string> | string | void 
   /** 是否处于生产环境 */
   isProduct?: boolean
   /** 缓存持续时间 */
   cacheExpire?: number
+  /** 缓存目录 */
+  cachePath?: string
+  /** 日志输出回调 */
+  logger?: Logger
 }
 type ServeYylSsr = (op: ServeYylSsrOption) => {}
 
