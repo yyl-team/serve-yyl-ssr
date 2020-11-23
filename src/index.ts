4757bd09fcf768a6cb4ee3ec04a2920c1e985ebf
liudaojie <liudaojie@yy.com>
2020-11-23 12:02:41 +0800

    feat: 输出 types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Req, Res } from './types'
 import { YylSsr, YylSsrOption } from './yylSsr'
-export { ssrRedis } from './redis'
+export * from './yylSsr'
+export * from './redis'
+export * from './types'
 export function serveYylSsr<O extends Res = Res, I extends Req = Req>(option: YylSsrOption<O, I>) {
   const ssr = new YylSsr(option)
   return ssr.apply()
